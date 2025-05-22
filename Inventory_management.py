f90d97c52443bb05a0e5bb63b22b59a2689044c6
Onyanga_Ambrose <amb.onyanga@gmail.com>
2025-05-22 15:33:38 +0300

    Worked on the Updating stock item as well as writing good error messages

--- Inventory_management.py
+++ Inventory_management.py
@@ -1,5 +1,6 @@
 def menu():
     global choice
+    print("\033[92mDASHBOARD\033[0m")
     print(f"Items in stock  : \033[92m{len(stock)}\033[0m")
     print(f"Items on order  : \033[92m{len(order)}\033[0m")
     print("\n\033[92mMenu Items, choose number!\033[0m")
@@ -8,6 +9,48 @@ def menu():
     print("3. Update an item on stock")
     print("4. Order an item")
     choice = int(input("\033[92mYour choice?: \033[0m"))
+    if choice == 1:
+        print("\033[92mLIST OF STOCK ITEMS\033[0m")
+        print(f"\033[92m{'SKU':<15} {'Name':<24} {'Price(UGX)':<12} {'Supplier'}\033[0m")
+        for i in range(len(stock)):
+            print(f"{stock[i]['sku']:<15} {stock[i]['name']:<24} {stock[i]['price']:<12} {stock[i]['supplier']}")
+        print("\n")
+        menu()
+    elif choice == 2:
+        sku = input("Enter the Item SKU: ")
+        name = input("Enter the Item Name: ")
+        ch = True
+        while ch == True:
+            try:
+                price = int(input("Enter the Item Price: "))
+                ch = False
+            except:
+                print("\033[91mPlease, enter the price in digits!\033[0m")
+                ch = True
+        supplier = input("Enter the Item Supplier: ")
+        stock.append({"sku": sku, "name": name, "price": price, "supplier": supplier})
+        print("\n\033[92mItem added to Stock successfully!\033[0m")
+        menu()
+    elif choice == 3:
+        sku1 = input("Enter the SKU of the stock item to update: ")
+        for i in range(len(stock)):
+            if sku1 in stock[i].values():
+                break
+            i = len(stock)
+        if i != len(stock):
+            sku = input("Enter the new SKU of the item: ")
+            name = input("Enter the new Name of the item: ")
+            price = int(input("Enter the new Price of the item: "))
+            supplier = input("Enter the new Supplier of the item: ")
+            stock[i] = {"sku": sku, "name": name, "price": price, "supplier": supplier}
+            print("\n\033[92mStock Item updated successfully!\033[0m\n")
+            menu()           
+        else:
+            print("\033[91m\nSorry, an item with that SKU does not exist!\033[0m")
+            menu()
+    else:
+        print("\033[91mWrong choice, try again!\033[0m")
+        menu()
 print("\n\033[92mINVENTORY MANAGEMENT SYSTEM\033[0m\n")
 stock = [
     {
@@ -44,18 +87,3 @@ stock = [
 
 order = []
 menu()
-if choice == 1:
-    print(f"\033[92m{'SKU':<15} {'Name':<24} {'Price(UGX)':<12} {'Supplier'}\033[0m")
-    for i in range(len(stock)):
-        print(f"{stock[i]['sku']:<15} {stock[i]['name']:<24} {stock[i]['price']:<12} {stock[i]['supplier']}")
-    print("\n")
-    menu()
-elif choice == 2:
-    sku = input("Enter the Item SKU: ")
-    name = input("Enter the Item Name: ")
-    price = int(input("Enter the Item Price: "))
-    supplier = input("Enter the Item Supplier: ")
-    stock.append({"sku": sku, "name": name, "price": price, "supplier": supplier})
-    print("\n\033[92mThank you for adding item on stock!\033[0m")
-    menu()
-    
