eff2a8c82d337203633ac955953a838427434aa4
Onyanga_Ambrose <amb.onyanga@gmail.com>
2025-05-22 15:49:01 +0300

    Added comments to my code for easy understanding

--- Inventory_management.py
+++ Inventory_management.py
@@ -1,15 +1,23 @@
 def menu():
     global choice
-    print("\033[92mDASHBOARD\033[0m")
+    print("\033[92mDASHBOARD\033[0m") #Extra characters exists to color the text
+    
+    # The following code is to display items on the dashboard such as in stock and on order!
     print(f"Items in stock  : \033[92m{len(stock)}\033[0m")
     print(f"Items on order  : \033[92m{len(order)}\033[0m")
+    
+    # This is the menu that a user can choose
     print("\n\033[92mMenu Items, choose number!\033[0m")
     print("1. Display a list of stock items")
     print("2. Add an item to stock")
     print("3. Update an item on stock")
     print("4. Order an item")
     choice = int(input("\033[92mYour choice?: \033[0m"))
+    
+    
+    #Conditional statements that shows the program how to proceed
     if choice == 1:
+        # Displaying the list of stock items
         print("\033[92mLIST OF STOCK ITEMS\033[0m")
         print(f"\033[92m{'SKU':<15} {'Name':<24} {'Price(UGX)':<12} {'Supplier'}\033[0m")
         for i in range(len(stock)):
@@ -17,8 +25,12 @@ def menu():
         print("\n")
         menu()
     elif choice == 2:
+        # Adding an item to stock
+        print("\033[92mAdding an item to stock\033[0m")
         sku = input("Enter the Item SKU: ")
         name = input("Enter the Item Name: ")
+        
+        # Here I handled errors of when a user enters price which are not integers
         ch = True
         while ch == True:
             try:
@@ -32,6 +44,8 @@ def menu():
         print("\n\033[92mItem added to Stock successfully!\033[0m")
         menu()
     elif choice == 3:
+        # Updating an item on stock!
+        print("\033[91mUpdating an item on stock!\033[0m")
         sku1 = input("Enter the SKU of the stock item to update: ")
         for i in range(len(stock)):
             if sku1 in stock[i].values():
@@ -40,7 +54,14 @@ def menu():
         if i != len(stock):
             sku = input("Enter the new SKU of the item: ")
             name = input("Enter the new Name of the item: ")
-            price = int(input("Enter the new Price of the item: "))
+            ch = True
+            while ch == True:
+                try:
+                    price = int(input("Enter the new Price of the item: "))
+                    ch = False
+                except:
+                    print("\033[91mPlease, enter the price in digits!\033[0m")
+                    ch = True
             supplier = input("Enter the new Supplier of the item: ")
             stock[i] = {"sku": sku, "name": name, "price": price, "supplier": supplier}
             print("\n\033[92mStock Item updated successfully!\033[0m\n")
@@ -52,6 +73,7 @@ def menu():
         print("\033[91mWrong choice, try again!\033[0m")
         menu()
 print("\n\033[92mINVENTORY MANAGEMENT SYSTEM\033[0m\n")
+# Below are hard coded stock items for demonstration purposes!
 stock = [
     {
         "sku": "MIL-APL1LTR",
