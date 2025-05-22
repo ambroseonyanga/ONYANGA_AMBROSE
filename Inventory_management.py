84a87cfb80679f4a8aca932e76c42e8e70cd0d53
Onyanga_Ambrose <amb.onyanga@gmail.com>
2025-05-22 10:15:30 +0300

    Initial commit of Inventory Management assignment

---
+++ Inventory_management.py
@@ -0,0 +1,8 @@
+
+print("\n\033[92mINVENTORY MANAGEMENT SYSTEM\033[0m\n")
+stock = []
+order = []
+transit = []
+print(f"Items in stock  : \033[92m{len(stock)}\033[0m")
+print(f"Items on order  : \033[92m{len(order)}\033[0m")
+print(f"Items in transit: \033[92m{len(transit)}\033[0m")
