af15a267c5f01e20d28ceeae05f27f15bd713970
Onyanga_Ambrose <amb.onyanga@gmail.com>
2025-05-22 23:13:11 +0300

    Made some edits and reduced the number of code

--- Inventory_management.py
+++ Inventory_management.py
@@ -1,20 +1,21 @@
 def menu():
-    global choice
     print("\033[92mDASHBOARD\033[0m") #Extra characters exists to color the text
-    
+        
     # The following code is to display items on the dashboard such as in stock and on order!
     print(f"Items in stock  : \033[92m{len(stock)}\033[0m")
-    print(f"Items on order  : \033[92m{len(order)}\033[0m")
-    
+    print(f"Items on order  : \033[92m{len(order)}\033[0m") 
+       
     # This is the menu that a user can choose
     print("\n\033[92mMenu Items, choose number!\033[0m")
     print("1. Display a list of stock items")
     print("2. Add an item to stock")
     print("3. Update an item on stock")
     print("4. Order an item")
-    choice = int(input("\033[92mYour choice?: \033[0m"))
-    
-    
+    try: 
+        choice = int(input("\033[92mYour choice?: \033[0m"))
+    except:
+        print("\033[91mPlease enter choice number instead!\033[0m")
+        menu()    
     #Conditional statements that shows the program how to proceed
     if choice == 1:
         # Displaying the list of stock items
@@ -23,7 +24,6 @@ def menu():
         for i in range(len(stock)):
             print(f"{stock[i]['sku']:<15} {stock[i]['name']:<24} {stock[i]['price']:<12} {stock[i]['supplier']}")
         print("\n")
-        menu()
     elif choice == 2:
         # Adding an item to stock
         print("\033[92mAdding an item to stock\033[0m")
@@ -41,10 +41,9 @@ def menu():
         supplier = input("Enter the Item Supplier: ")
         stock.append({"sku": sku, "name": name, "price": price, "supplier": supplier})
         print("\n\033[92mItem added to Stock successfully!\033[0m")
-        menu()
     elif choice == 3:
         # Updating an item on stock!
-        print("\033[91mUpdating an item on stock!\033[0m")
+        print("\033[92mUpdating an item on stock!\033[0m")
         sku1 = input("Enter the SKU of the stock item to update: ")
         for i in range(len(stock)):
             if sku1 in stock[i].values():
@@ -63,15 +62,11 @@ def menu():
                     ch = True
             supplier = input("Enter the new Supplier of the item: ")
             stock[i] = {"sku": sku, "name": name, "price": price, "supplier": supplier}
-            print("\n\033[92mStock Item updated successfully!\033[0m\n")
-            menu()           
+            print("\n\033[92mStock Item updated successfully!\033[0m\n")          
         else:
             print("\033[91m\nSorry, an item with that SKU does not exist!\033[0m")
-            menu()
-    elif choice == 4:
-        
-        # This is the logic for ordering an item.
-        # Notice the dashboard changes accordingly
+    elif choice == 4:        
+        # This is the logic for ordering an item. Notice the dashboard changes accordingly        
         sku = input("Enter the SKU of the item you want to order: ")
         for i in range(len(stock)):
             if sku in stock[i].values():
@@ -81,14 +76,11 @@ def menu():
             order.append({"sku": stock[i]["sku"], "name": stock[i]["name"], "price": stock[i]["price"], "supplier": stock[i]["supplier"]})
             del stock[i]
             print("\n\033[92mItem ordered successfully\033[0m")
-            menu()
         else:
-            print("\033[91mSorry, that item is out of stock\033[0m")
-            menu()
-        
+            print("\033[91mSorry, that item is out of stock\033[0m")        
     else:
         print("\033[91mWrong choice, try again!\033[0m")
-        menu()
+    menu()
 print("\n\033[92mINVENTORY MANAGEMENT SYSTEM\033[0m\n")
 
 # Below are hard coded stock items for demonstration purposes!
@@ -116,14 +108,13 @@ stock = [
         "name": "Yakult Strawberry",
         "price": 6000,
         "supplier": "Yakult Kenya"
-    },
-    {
+    }
+]
+
+order = [{
         "sku": "BIS-OREO123",
         "name": "Oreo Biscuits (123g)",
         "price": 4000,
         "supplier": "Mondelez Suppliers"
-    }
-]
-
-order = []
+    }]
 menu()
