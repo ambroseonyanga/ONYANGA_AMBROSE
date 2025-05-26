da148e354fa508102603d883d7d4b0a412f3f388
Onyanga_Ambrose <amb.onyanga@gmail.com>
2025-05-26 13:45:15 +0300

    Added training session

--- practice1.py
+++ practice1.py
@@ -1,1 +1,9 @@
-print("\033[92;1mAmbrose\033[0m is good")
+class Animal:
+    def speak(self):
+        print("This animal makes sound")
+class Cat(Animal):
+    def sound(self):
+        print("The cat meows")
+cat1 = Cat()
+cat1.sound()
+cat1.speak()
