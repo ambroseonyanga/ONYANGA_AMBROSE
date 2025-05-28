9a886783bed73c18bf318d8c87ead0b102ab1c27
Onyanga_Ambrose <amb.onyanga@gmail.com>
2025-05-28 12:21:46 +0300

    Added assignment for overriding

---
+++ datascience.py
@@ -0,0 +1,2 @@
+# Data science: Methods that help us to extract data from insights(Structured data-excel sheets, 
+# unstructured data-images)
--- practice1.py
+++ practice1.py
@@ -9,23 +9,48 @@
 # cat1.speak()
 
 
-class Bird:
-    def fly(self):
-        print("Birds can fly!")
+# class Bird:
+#     def fly(self):
+#         print("Birds can fly!")
 
-class Eagle(Bird):
-    def fly(self):
-        print("An eagle can fly at an high altitude!")
-class Hen(Bird):
-    def fly(self):
-        print("A hen can fly at a low altitude!")
-def specific(bird):
-    bird.fly()
+# class Eagle(Bird):
+#     def fly(self):
+#         print("An eagle can fly at an high altitude!")
+# class Hen(Bird):
+#     def fly(self):
+#         print("A hen can fly at a low altitude!")
+# def specific(bird):
+#     bird.fly()
 
-bird = Bird()
-bird1 = Hen()
-bird2 = Eagle()
+# bird = Bird()
+# bird1 = Hen()
+# bird2 = Eagle()
 
-specific(bird)
-specific(bird2)
-specific(bird1)
+# specific(bird)
+# specific(bird2)
+# specific(bird1)
+
+
+
+
+# from abc import ABC, abstractmethod #abstraction
+# class Person(ABC):
+#     @abstractmethod
+#     def name(self):
+#         pass
+# class Person1(Person):
+#     def sing(self):
+#         print("I can sing")
+#     def name(self):
+#         print("Hello, I am ")
+# amb = Person1()
+# amb.sing()
+
+
+def prod():
+    print("This is the first method")
+def prod(x):
+    print("This is the last method")
+    
+prod()
+prod("Ambrose")
